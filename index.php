202060b298158620b791397cd3f113d829ed20f0
Dylan Wood <dwood@lintcoin.mind.unm.edu>
2014-02-12 10:40:57 -0700

    modified to use phptidy instead of php_beautifier

--- index.php
+++ index.php
@@ -3,10 +3,11 @@ require_once 'PHP/Beautifier.php';
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ob_start();
-$tmp_filename = '/tmp/beautify_me.php' . microtime();
+$tmp_filename = '/tmp/tidy_me.php' . microtime();
 //get uploaded file
 if ($filename = get_uploaded_file($tmp_filename)) {
    $uploaded_filename = get_uploaded_file_name();
+   $new_filename = tidy($filename);
    ob_clean();
    header('Content-Description: File Transfer');
    header('Content-Type: application/octet-stream');
@@ -14,29 +15,24 @@ if ($filename = get_uploaded_file($tmp_filename)) {
    header('Expires: 0');
    header('Cache-Control: must-revalidate');
    header('Pragma: public');
-   beautify($filename);
+   echo file_get_contents($new_filename);
    ob_end_flush();
    unlink($tmp_filename);
+   unlink($new_filename);
 } else {
    //there was a problem with the uploaded file.
    reply_with_error('Could not locate uploaded file');
    die;
 }
-function beautify($filename) {
-   // Create the instance
-   $oBeautifier = new PHP_Beautifier();
-   // Add another filter, with one parameter
-   //$oBeautifier->addFilter('Pear', array('add_header' => 'php'));
-   // Set the indent char, number of chars to indent and newline char
-   $oBeautifier->setIndentChar(' ');
-   $oBeautifier->setIndentNumber(3);
-   $oBeautifier->setNewLine("\n");
-   // Define the input file
-   $oBeautifier->setInputFile($filename);
-   // Process the file. DON'T FORGET TO USE IT
-   $oBeautifier->process();
-   // Show the file (echo to screen)
-   $oBeautifier->show();
+function tidy($filename) {
+   $tidy_path = '/usr/share/php/PHP/phptidy/phptidy.php';
+   chmod($filename, '777');
+   exec('php ' . $tidy_path . ' suffix ' 
+      . escapeshellarg($filename), $outputs, $return_var);
+   //DEBUG
+   //error_log(print_r($outputs), true);
+   $safe_output = trim($outputs[4]);
+   return str_replace(' saved.', '', $safe_output);
 }
 function get_uploaded_file_name() {
    if (!empty($_GET['test'])) {
